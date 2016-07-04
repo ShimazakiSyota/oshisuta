112725f4f6dcbf7ef00e2d0fee3b443d62b23728
ShimazakiSyota <1401010@st.asojuku.ac.jp>
2016-07-04 16:20:13 +0900

    noda

--- jobdetail.php
+++ jobdetail.php
@@ -1,7 +1,5 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-	"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-	<?php
+﻿<html>
+<?php
 
 	require_once 'DBmanager.php';//クラスファイル呼び出し
 
@@ -26,7 +24,7 @@
 
 $jobid=$_POST['jobid'];
 //---------------------------------------------------------------------------------------------------
-	?>
+?>
 
 <head>
 
